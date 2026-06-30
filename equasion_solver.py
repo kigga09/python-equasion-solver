730f2f81d676ff27923029038164aac01ed1b515
khalil <khalilbelgh09@gmail.com>
2026-06-30 20:26:24 +0100

    fixed the error

--- equasion_solver.py
+++ equasion_solver.py
@@ -5,17 +5,17 @@ deg = int(input("Enter the degree of the polynomial: "))
 
 if deg == 1:
     print('enter the coefficients of the polynomial in the form ax + b = 0')
-    a = float(input("Enter the coefficient a: "))
-    b = float(input("Enter the coefficient b: "))
+    a = float(input("a= "))
+    b = float(input("b= "))
     if a == 0:
         print("error: coefficient a cannot be zero for a linear equation.")
     else:
-       return b / a
+       print(f"The solution to the equation is: x = {-b/a}")
 elif deg == 2:
     print('enter the coefficients of the polynomial in the form ax^2 + bx + c = 0')
-    a = float(input("Enter the coefficient a: "))
-    b = float(input("Enter the coefficient b: "))
-    c = float(input("Enter the coefficient c: "))
+    a = float(input("a= "))
+    b = float(input("b= "))
+    c = float(input("c= "))
     if a == 0:
         print("error: coefficient a cannot be zero for a quadratic equation.")
     else:
