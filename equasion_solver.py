d89d5ec1861051bc04a45201a2dd0159709a2d85
khalil <khalilbelgh09@gmail.com>
2026-06-30 20:46:15 +0100

    .

--- equasion_solver.py
+++ equasion_solver.py
@@ -2,24 +2,25 @@
 
 from math import sqrt
 
+from math import sqrt
+
+print("This program solves polynomial equations of degree 1 and 2.")
 deg = int(input("Enter the degree of the polynomial: "))
 
-print("""Enter the coefficients of the polynomial in decreasing order of degree such as
-      a*x^2 + b*x + c = 0""")
-
-a = float(input('a='))
-b = float(input('b='))
-c = float(input('c='))
-
-delta = b**2 - 4*a*c
-
-def solve_quadratic(a, b, c):
-    if delta > 0 :
-        x1 = (-b + delta**0.5) / (2*a)
-        x2 = (-b - delta**0.5) / (2*a)
-        return x1, x2
-    elif delta == 0:
-        x = -b / (2*a)
-        return x,
+if deg == 1:
+    print('enter the coefficients of the polynomial in the form ax + b = 0')
+    a = float(input("a= "))
+    b = float(input("b= "))
+    if a == 0:
+        print("error: coefficient a cannot be zero for a linear equation.")
     else:
-        return 'No real roots'
+       print(f"The solution to the equation is: x = {-b/a}")
+elif deg == 2:
+    print('enter the coefficients of the polynomial in the form ax^2 + bx + c = 0')
+    a = float(input("a= "))
+    b = float(input("b= "))
+    c = float(input("c= "))
+    if a == 0:
+        print("error: coefficient a cannot be zero for a quadratic equation.")
+    else:
+        print('No real roots') 
