dfe5ab36993578c608e35476999dae559910ae90
khalil <khalilbelgh09@gmail.com>
2026-06-30 13:53:29 +0100

    the user can enter the coefficients of his polynomial

--- equasion_solver.py
+++ equasion_solver.py
@@ -2,3 +2,10 @@
 
 deg = int(input("Enter the degree of the polynomial: "))
 
+print("""Enter the coefficients of the polynomial in decreasing order of degree such as
+      a*x^2 + b*x + c = 0""")
+
+a = float(input('a='))
+b = float(input('b='))
+c = float(input('c='))
+
