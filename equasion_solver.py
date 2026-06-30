6b32636ad397b43f888ece6ef2aa867f6a40ee4f
khalil <khalilbelgh09@gmail.com>
2026-06-30 14:21:00 +0100

    add the function that gives all real solutions

--- equasion_solver.py
+++ equasion_solver.py
@@ -9,3 +9,15 @@ a = float(input('a='))
 b = float(input('b='))
 c = float(input('c='))
 
+delta = b**2 - 4*a*c
+
+def solve_quadratic(a, b, c):
+    if delta > 0 :
+        x1 = (-b + delta**0.5) / (2*a)
+        x2 = (-b - delta**0.5) / (2*a)
+        return x1, x2
+    elif delta == 0:
+        x = -b / (2*a)
+        return x,
+    else:
+        return 'No real roots'
